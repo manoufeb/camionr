bd00ca6244e8a6e7a06b66ebe1356920d9c11259
Mael Singh <maelsingh@gmail.com>
2016-08-18 18:58:19 +0200

    tooltips

--- app/assets/javascripts/trucks.js
+++ app/assets/javascripts/trucks.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$("#myform :input").tooltip({
+
+      // place tooltip on the right edge
+      position: "center right",
+
+      // a little tweaking of the position
+      offset: [-2, 10],
+
+      // use the built-in fadeIn/fadeOut effect
+      effect: "fade",
+
+      // custom opacity setting
+      opacity: 0.7
+
+      });
