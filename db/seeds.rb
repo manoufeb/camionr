179217fba5259469c6c40de8db55283e92f6dfc8
Manou <mfebvret@gmail.com>
2016-08-18 19:45:49 +0200

    delete seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-Truck.destroy_all
-Booking.destroy_all
+# User.destroy_all
+# Truck.destroy_all
+# Booking.destroy_all
 
-customer = User.create(email: "customer@test.com", password: "123456")
-supplier = User.create(email: "supplier@test.com", password: "123456")
+# customer = User.create(email: "customer@test.com", password: "123456")
+# supplier = User.create(email: "supplier@test.com", password: "123456")
 
-truck = Truck.create(user: supplier, gauge: "8x4", category: "Grue", numberplate: "867 RJB 34", price: 870, description: "Très beau camion", availability: true)
+# truck = Truck.create(user: supplier, gauge: "8x4", category: "Grue", numberplate: "867 RJB 34", price: 870, description: "Très beau camion", availability: true)
 
-booking = Booking.create(location: "Paris", starting_date: Date.new(2016, 8, 18), ending_date: Date.new(2016, 8, 20), truck: truck, user: customer)
