bf03cae31e2c26284e0129c2d755966b12dc429a
Paul HERRENSCHMIDT <herrenschmidtpaul@gmail.com>
2016-08-18 18:57:54 +0200

    avatar and forms finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :company, :siret, :address, :avatar, :avatar_cache)
+  end
 end
 
