503ccadea8d2f1d45a602d1030fda72dddf92f49
Manou <mfebvret@gmail.com>
2016-08-19 12:00:12 +0200

    pending booking show done

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,16 +1,16 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  process eager: true  # Force version generation at upload time.
+  # process eager: true  # Force version generation at upload time.
 
-  process convert: 'jpg'
+  # process convert: 'jpg'
 
-  version :standard do
-    resize_to_fit 800, 600
-  end
+  # version :standard do
+  #   resize_to_fit 800, 600
+  # end
 
-  version :bright_face do
-    cloudinary_transformation effect: "brightness:30", radius: 20,
-      width: 150, height: 150, crop: :thumb, gravity: :face
-  end
+  # version :bright_face do
+  #   cloudinary_transformation effect: "brightness:30", radius: 20,
+  #     width: 150, height: 150, crop: :thumb, gravity: :face
+  # end
 end
